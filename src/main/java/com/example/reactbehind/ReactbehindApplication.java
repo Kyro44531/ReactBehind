d2e8f9a5994ea8687c8e61cc72130627915973cf
peppa <kyro@DESKTOP-6CA1TIV>
2024-08-17 13:08:08 +0800

    Save

---
+++ src/main/java/com/example/reactbehind/ReactbehindApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactbehind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactbehindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactbehindApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reactbehind/ReactbehindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactbehind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactbehindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
